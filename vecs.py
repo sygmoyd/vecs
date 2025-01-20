f529adbd9d3868887ea3526ae7fd8b7db49a27f9
sygmoyd <118548706+sygmoyd@users.noreply.github.com>
2025-01-20 13:01:33 +0100

    Create vecs.py
    -creation of numerical vectors
    -vector addition
    -dotProduct
    -l1 Norm
    -l2 Norm

---
+++ vecs.py
@@ -0,0 +1,33 @@
+class vec:
+    def __init__(self, value=[]):
+        if value == []: self.value = [];return 
+        if not all(isinstance(item, (int, float)) for item in value):
+            raise TypeError("all elements must be ints, floats or bools!")
+        self.value = value
+
+    def __add__(self, vec2):
+        if not len(self.value) == len(vec2.value): raise ValueError("vectors must have the same length")
+        value = [a+b for a, b in zip(self.value, vec2.value)]
+        return vec(value)
+    
+    def dotP(self, vec2):
+        val2 = vec2
+        if type(vec2) is vec:
+            val2 = vec2.value
+        s = 0
+        for a,b in zip(self.value, val2):
+            s += a*b
+        return s
+
+    def l1Norm(self):
+        s = 0
+        for i in self.value:
+            s += abs(i)
+        return s
+    
+    def l2Norm(self):
+        s = self.dotP(self.value)
+        return s ** 0.5
+    
+v = vec([1])
+print(v.l2Norm())
